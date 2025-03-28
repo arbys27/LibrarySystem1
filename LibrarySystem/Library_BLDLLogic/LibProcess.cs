9cf881cd2e42aae20636480a5a1fb95c4e26b673
arbys27 <arbybarnuevo27@gmail.com>
2025-03-29 03:36:27 +0800

    separating

---
+++ LibrarySystem/Library_BLDLLogic/LibProcess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library_BLDLLogic
+{
+    public class LibProcess
+    {
+        
+        public static bool UpdateBook()
+        {
+            ViewBooks();
+            Console.Write("Enter book number to update: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= books.Count)
+            {
+                Console.Write("Enter new book title: ");
+                string newTitle = Console.ReadLine();
+                books[index - 1] = newTitle;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+        
+    }
+}
